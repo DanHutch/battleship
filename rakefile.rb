10e1f684d742f553f268f69b1ac01e08c945a9bd
DanHutch <danhutch5280@gmail.com>
2018-08-31 20:37:53 -0600

    initial commit

---
+++ rakefile.rb
@@ -0,0 +1,7 @@
+## rakefile.rb
+
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.pattern = "test/**/*_test.rb"
+end
---
+++ test/battleship_test.rb
@@ -0,0 +1,5 @@
+## battleship_test.rb
+
+require './test/test_helper'
+require './lib/battleship'
+require 'pry'
