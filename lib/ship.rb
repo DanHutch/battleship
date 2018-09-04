f347d32bf868a06ef5f6ce01d65b903d181d09ef
DanHutch <danhutch5280@gmail.com>
2018-09-03 22:11:34 -0600

    completed ship class and tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -10,29 +10,19 @@ class Ship
               :spaces,
               :damage
 
-  def initialize(owner, length, spaces)
+  def initialize(owner, spaces)
     @owner = owner
-    @length = length
+    @length = spaces.count
     @spaces = spaces
     @damage = 0
   end
 
-# I need to set some validating methods somehow...
-# => owner must be (player / npc)
-# => length must be an integer and must be (2 / 3)
-# => spaces must be an array of strings
-# => => need to have validation conditions for the strings
-# => spaces.count must be equal to length
+  def hit
+    @damage += 1
+  end
 
-  def occupy
-    binding.pry
-    @spaces.each do |space|
-      if @owner == "npc"
-       Board.npc_map[space].npc_occupy
-      elsif @owner == "player"
-       Board.player_map[space].player_occupy
-      end
-    end
+  def sunk?
+    @damage == @length
   end
 
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,37 +7,40 @@ require './lib/board'
 class ShipTest < Minitest::Test
 
   def test_it_exists
-    ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
     assert_instance_of(Ship, ship)
   end
 
   def test_it_has_an_owner
-    ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
     assert_equal("npc", ship.owner)
   end
 
   def test_it_has_a_length
-    ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
-    assert_equal(2, ship.length)
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
+    assert_equal(3, ship.length)
   end
 
   def test_it_has_spaces
-    ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
     assert_equal(["A1", "A2", "A3"], ship.spaces)
   end
 
   def test_it_starts_with_no_damage
-    ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
     assert_equal(0, ship.damage)
   end
 
-  # def test_it_can_occupy_those_spaces
-  #   board = Board.new
-  #   ship = Ship.new("npc", 2, ["A1", "A2", "A3"])
-  #   ship.occupy
-  #   assert_equal(true, board.npc_map["A1"].occupied)
-  #   assert_equal(true, #space2.occupied)
-  #   assert_equal(true, #space3.occupied)
-  # end
+  def test_it_can_be_damaged
+    ship = Ship.new("npc", ["A1", "A2", "A3"])
+    assert_equal(0, ship.damage)
+    ship.hit
+    assert_equal(1, ship.damage)
+    ship.hit
+    assert_equal(2, ship.damage)
+    ship.hit
+    assert_equal(3, ship.damage)
+    assert(ship.sunk?)
+  end
 
 end
