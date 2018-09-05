f101483f9a4a1d565da91c0a62b93c4d59b1b907
DanHutch <danhutch5280@gmail.com>
2018-09-05 17:42:25 -0600

    finished random spaces functionality

--- lib/random_spaces.rb
+++ lib/random_spaces.rb
@@ -1,16 +1,41 @@
 ## random_spaces.rb
 
+require './lib/ship_validation'
 require 'pry'
 
 class RandomSpaces
-  attr_reader :length,
-              :spaces
+  attr_reader :coordinates
 
-  def initialize(length)
-    @length = length
-    @spaces = []
+  def initialize
+    @coordinates = []
   end
 
-  
+  def random_2_set
+    sets = ShipValidation.new(2).valid_sets
+    @coordinates << sets.sample
+  end
+
+  def random_3_set
+    sets = ShipValidation.new(3).valid_sets
+    valid_set = []
+    loop do
+      current_set = sets.sample
+      if valid?(current_set)
+        valid_set = current_set
+        break
+      end
+    end
+    @coordinates << valid_set
+  end
+
+  def valid?(set)
+    set.map do |space|
+      if @coordinates.include?(set)
+        false
+      else
+        true
+      end
+    end
+  end
 
 end
